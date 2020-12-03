069a8e8406de988dfbdb24bdba3ac0d04625f11f
libanesse <61927607+libanesse@users.noreply.github.com>
2020-12-03 00:42:36 -0600

    initial commit

---
+++ userdirectory/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Directory />
+    </div>
+  );
+};
+
+
+export default App;
