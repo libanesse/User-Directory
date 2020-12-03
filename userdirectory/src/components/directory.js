d1538dbb74087d50f8e0efa06e24bad214be29f8
Liban Esse <61927607+libanesse@users.noreply.github.com>
2020-12-03 01:54:46 -0600

    Update directory.js

--- userdirectory/src/components/directory.js
+++ userdirectory/src/components/directory.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Employees from "./Employees.js";
 import Search from "./Search.js";
 import API from "../utils/API";
-//import "../styles/Directory.css";
+
 
 
 
